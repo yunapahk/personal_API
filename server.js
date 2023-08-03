17354ae83f24d93dc980ad08307ec5e3878b3323
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-08-02 22:35:05 -0700

    Schema changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 require("dotenv").config();
 // pull PORT from .env, give default value of 3000
 // pull MONGODB_URL from .env
-const { PORT = 3500, DATABASE_URL } = process.env;
+const { PORT = 3000, DATABASE_URL } = process.env;
 // import express
 const express = require("express");
 // create application object
@@ -33,95 +33,99 @@ mongoose.connection
 // ///////////////////////////////
 // MODELS
 // ////////////////////////////////
-const StockSchema = new mongoose.Schema({
-  name: String,
+const movieSchema = new mongoose.Schema({
   title: String,
-  image: String
+  director: String,
+  year: Number,
+  image: String,
+
 });
 
-const Stock = mongoose.model("Stock", StockSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 
-// ///////////////////////////////
-// // MiddleWare
-////////////////////////////////
-app.use(cors()); // to prevent cors errors, open access to all origins
-app.use(morgan("dev")); // logging
-app.use(express.json()); // parse json bodies
+//////////////////////////////
+// Middleware
+//////////////////////////////
+// cors for preventing cors errors (allows all requests from other origins)
+app.use(cors())
+// morgan for logging requests
+app.use(morgan("dev"))
+// express functionality to recognize incoming request objects as JSON objects
+app.use(express.json())
 
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 // create a test route
 app.get("/", (req, res) => {
-  res.send("hello world");
-});
+  res.json({hello: "world"})
+})
 
-// INDEX - GET - /stocks - gets all stocks
-app.get("/stock", async (req, res) => {
-    try {
-      // fetch all stock from database
-      const stock = await Stock.find({});
-      // send json of all stocks
-      res.json(stock);
-    } catch (error) {
-      // send error as JSON
-      res.status(400).json({ error });
-    }
-  });
-
-// CREATE - POST - /stock - create a new person
-app.post("/stock", async (req, res) => {
-    try {
-        // create the new person
-        const person = await Stock.create(req.body)
-        // send newly created person as JSON
-        res.json(stock)
-    }
-    catch(error){
-        res.status(400).json({ error })
-    }
-});
-
-
-// SHOW - GET - /stock/:id - get one stock by id
-app.get("/stock/:id", async (req, res) => {
-        try {
-            // fetch all stocks from database
-            const stock = await Stock.findById(req.params.id);
-        } catch (error) {
-            res.status(400).json({ error });
-    }
-});
-
-// UPDATE ROUTE
-app.put("/stock/:id", async (req, res) => {
+// INDEX - GET - /movie - gets all movies
+app.get("/movie", async (req, res) => {
   try {
-    // send all stock
-    const stock = await Stock.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        });
-    res.json(
-      await Stock.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    );
+    // fetch all movies from database
+    const movie = await Movie.find({});
+    // send json of all movies
+    res.json(movie);
   } catch (error) {
-    //send error
-    res.status(400).json(error);
+    // send error as JSON
+    res.status(400).json({ error });
   }
 });
 
-// DESTROY - DELETE - /stock/:id - delete a stock person
-app.delete("/stock/:id", async (req, res) => {
-    try {
-        // delete the person
-        const stock = await Stock.findByIdAndDelete(req.params.id)
-        // send deleted stock as json
-        res.status(204).json(stock)
-    } catch(error){
-        res.status(400).json({error})
-    }
+// CREATE - POST - /movie - create a new movie
+app.post("/movie", async (req, res) => {
+  try {
+      // create the new movie
+      const movie = await Movie.create(req.body)
+      // send newly created movie as JSON
+      res.json(movie)
+  }
+  catch(error){
+      res.status(400).json({ error })
+  }
+})
+
+// SHOW - GET - /movie/:id - get a single movie
+app.get("/movie/:id", async (req, res) => {
+  try {
+    // get a movie from the database
+    const movie = await Movie.findById(req.params.id);
+    // return the movie as json
+    res.json(movie);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+// UPDATE - PUT - /movie/:id - update a single movie
+app.put("/movie/:id", async (req, res) => {
+  try {
+    // update the movie
+    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    // send the updated movie as json
+    res.json(movie);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+// DESTROY - DELETE - /movie/:id - delete a single movie
+app.delete("/movie/:id", async (req, res) => {
+  try {
+      // delete the movie
+      const movie = await Movie.findByIdAndDelete(req.params.id)
+      // send deleted movie as json
+      res.status(204).json(movie)
+  } catch(error){
+      res.status(400).json({error})
+  }
 })
 
 ///////////////////////////////
 // LISTENER
 ////////////////////////////////
-app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Iron Man says ily ${PORT}`));
